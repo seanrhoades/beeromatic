02bec461a1199cb4cb326d912cf26d8419961ecf
Sean Rhoades <sean@seanrhoades.com>
2016-02-25 16:42:36 -0500

    moved httparty gem

--- app/controllers/beer_time_controller.rb
+++ app/controllers/beer_time_controller.rb
@@ -1,4 +1,4 @@
-class BeerTimeController < ApplicationController
+class BeerTimeController<ApplicationController
   def index
       @randombeer = HTTParty.get('http://api.brewerydb.com/v2/beer/random?hasLabels=Y&withBreweries=Y&key=1f754235e3e5aad91613ca4acf567170'
       )
